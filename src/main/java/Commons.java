d684df0ae795d31d148776ce3dfdf845da155cba
yenthehaiquang <155195199+yenthehaiquang@users.noreply.github.com>
2024-01-12 21:08:07 +0700

    Create Commons.java

---
+++ src/main/java/Commons.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author
+ */
+public interface Commons {
+
+    public static final int BOARD_WIDTH = 1200;
+    public static final int BOARD_HEIGTH = 800;
+    public static final int GROUND = 650;
+    public static final int BOMB_HEIGHT = 5;
+    public static final int ALIEN_HEIGHT = 40;
+    public static final int ALIEN_WIDTH = 40;
+    public static final int BORDER_RIGHT = 30;
+    public static final int BORDER_LEFT = 5;
+    public static final int GO_DOWN = 25;
+    public static final int NUMBER_OF_ALIENS_TO_DESTROY = 18;
+    public static final int CHANCE = 5;
+    public static final int DELAY = 17;
+    public static final int PLAYER_WIDTH = 50;
+    public static final int PLAYER_HEIGHT = 50;
+
+}
